4f0d7806e5fe709f4c09096b6f0d322f3fda7486
Sarah Wadley <lifeunsubscribe@gmail.com>
2026-03-29 14:11:55 -0600

    wip: auto-commit relevant changes for issue #368 (2026-03-29)

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -6,6 +6,7 @@
 # - grocery.py (1F)
 # - prepared_foods.py (1G)
 # - scraper.py (2C - scraper/import endpoints)
+# - tasks.py (2A - async task status endpoints)
 
 from src.routers.auth import router as auth_router
 from src.routers.users import router as users_router
@@ -15,5 +16,6 @@ from src.routers.recipes import router as recipes_router
 from src.routers.grocery import router as grocery_router
 from src.routers.prepared_foods import router as prepared_foods_router
 from src.routers.scraper import router as scraper_router
+from src.routers.tasks import router as tasks_router
 
-__all__ = ["auth_router", "users_router", "substitutions_router", "inventory_router", "recipes_router", "grocery_router", "prepared_foods_router", "scraper_router"]
+__all__ = ["auth_router", "users_router", "substitutions_router", "inventory_router", "recipes_router", "grocery_router", "prepared_foods_router", "scraper_router", "tasks_router"]
