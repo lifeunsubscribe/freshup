3d6c9e4000350bee6229cd87dae669a7d872e284
Sarah Wadley <lifeunsubscribe@gmail.com>
2026-04-03 14:11:04 -0600

    fix: restore tasks_router import (this branch's feature, not receipts)

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -15,5 +15,6 @@ from src.routers.recipes import router as recipes_router
 from src.routers.grocery import router as grocery_router
 from src.routers.prepared_foods import router as prepared_foods_router
 from src.routers.scraper import router as scraper_router
+from src.routers.tasks import router as tasks_router
 
-__all__ = ["auth_router", "users_router", "substitutions_router", "inventory_router", "recipes_router", "grocery_router", "prepared_foods_router", "scraper_router"]
+__all__ = ["auth_router", "users_router", "substitutions_router", "inventory_router", "recipes_router", "grocery_router", "prepared_foods_router", "scraper_router", "tasks_router"]
